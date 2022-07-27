ec68b5422c251bf30208b440042caeb9583467db
Gaston <lioneltamantini@gmail.com>
2022-07-27 18:56:34 -0300

    fin

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
   useEffect(() => {
     const obtenerClientes = async () => {
       try {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_URL;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
@@ -34,7 +34,7 @@ const Inicio = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          const url = `http://localhost:4000/clientes/${id}`;
+          const url = `${import.meta.env.VITE_API_URL}/${id}`;
           const respuesta = await fetch(url, {
             method: "DELETE",
           });
